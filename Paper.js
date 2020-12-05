5b44cf22d715ba458b2578b0ebc53219554acb31
SrushtiNandurkar <73391859+SrushtiNandurkar@users.noreply.github.com>
2020-12-05 18:06:14 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,29 @@
+class paper{
+	constructor(x,y,radius){
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0.5,
+			density:1.2
+			}
+		this.x=x;
+		this.y=y;
+		this.radius=radius;
+		this.height = 55;
+		this.paperimage = loadImage("paper.png");
+		this.body=Bodies.circle(this.x, this.y, this.radius/2, options);
+		World.add(world, this.body);
+	}
+	display(){
+	 var paperpos=this.body.position;		
+     push();
+	 translate(paperpos.x, paperpos.y);
+	 rectMode(CENTER);
+	 fill(230);
+	 stroke(230);
+	 imageMode(CENTER);
+	 image(this.paperimage,0,0,this.radius,this.height);
+	 pop();
+	}
+
+}
